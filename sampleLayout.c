1f098a536cb298e70823e7d527e9e3012cba0918
JackDixon-W <jdixon.dixon04@gmail.com>
2024-11-12 12:09:01 +0000

    Added sampleLayout file

---
+++ sampleLayout.c
@@ -0,0 +1,23 @@
+/*
+Author: Jack Dixon
+Description:
+Date: 
+*/
+
+// Included Libraries
+#include <stdlib.h>
+#include <stdio.h>
+//#include <string.h>
+
+// Type Declarations
+
+// Structure Declarations
+
+// Function Prototypes
+
+// Main program
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
